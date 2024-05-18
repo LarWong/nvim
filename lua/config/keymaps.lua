a4feeb1923a81a98f953c52b753e0ccefc2ce293
Larry Wong <33496254+LarWong@users.noreply.github.com>
2024-05-18 18:04:48 -0400

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- jump to bottom and keep cursor in center
+map("n", "G", "Gzz", { silent = true })
+
+-- do not replace clipboard when pasting in V mode
+map("v", "p", '"_dP', { silent = true })
+
+-- term mode: esc to exit insert
+map("t", "<Esc>", "<C-\\><C-n>", { silent = true })
