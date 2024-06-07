c5b341942fb89151a620a6667834eec6b7967827
Larry Wong <33496254+LarWong@users.noreply.github.com>
2024-06-07 00:38:46 -0400

    Update lspconfig.lua

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,9 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    diagnostics = {
+      virtual_text = false,
+    },
     setup = {
       clangd = function(_, opts)
         opts.capabilities.offsetEncoding = { "utf-16" }
